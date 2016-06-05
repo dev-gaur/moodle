09b67414541974178c267fafddf450bacfd5e9c7
Devang Gaur <devang.only@gmail.com>
2016-06-06 01:57:57 +0530

    declared basic code structure

---
+++ user/classes/search/user.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU  General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Search area for Users.
+ *
+ * @package    core_user
+ * @copyright  2016 Devang Gaur {@link http://www.devanggaur.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace core_user\search;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Search area for Users.
+ *
+ * @package    core_user
+ * @copyright  2016 Devang Gaur {@link http://www.devanggaur.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class user extends \core_search\area\base {
+
+	public function get_recordset_by_timestamp($modifiedfrom = 0) {
+		
+	}
+
+	public function get_document(){
+		
+	}
+		
+	public function check_access(){
+			
+	}
+	
+	public function get_doc_url(){
+		
+	}
+	
+	public function get_context_url(){
+		
+	}
+}
